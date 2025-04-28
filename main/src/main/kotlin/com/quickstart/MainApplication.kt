85f0f0a44ba9efe030148e9c2cab52196b670369
Enrico Leonardo Orlando <enricolo4@gmail.com>
2025-04-28 14:21:17 -0300

    feat: Configuração inicial da aplicação Quarkus
    Adiciona a classe principal da aplicação Quarkus (`MainApplication`) anotada com `@QuarkusMain`.
    
    Inclui um bean de ciclo de vida (`AppLifecycleBean`) para logar uma mensagem durante a inicialização da aplicação usando `@Observes StartupEvent`.
    
    Define a função `main` padrão para iniciar a aplicação Quarkus via `Quarkus.run`.

--- main/src/main/kotlin/com/quickstart/MainApplication.kt
+++ main/src/main/kotlin/com/quickstart/MainApplication.kt
@@ -1,12 +1,14 @@
 package com.quickstart
 
+import io.quarkus.runtime.Quarkus
 import io.quarkus.runtime.StartupEvent
+import io.quarkus.runtime.annotations.QuarkusMain
 import jakarta.enterprise.context.ApplicationScoped
 import jakarta.enterprise.event.Observes
 import org.slf4j.LoggerFactory
 
 @ApplicationScoped
-class MainApplication {
+class AppLifecycleBean {
     fun onStartup(@Observes event: StartupEvent) {
         logger.info("A aplicação Quarkus está iniciando a partir do módulo main...")
     }
@@ -14,4 +16,11 @@ class MainApplication {
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java)
     }
+}
+
+@QuarkusMain
+class MainApplication
+
+fun main(args: Array<String>) {
+    Quarkus.run(*args)
 }
